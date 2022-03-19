b52017ddd2b35d357b09b563885457deeb171a9f
kaznak <kaznak.at.work@gmail.com>
2022-03-19 23:57:46 +0900

    update

--- fixed_point_number/fixed_point_number.py
+++ fixed_point_number/fixed_point_number.py
@@ -4,3 +4,21 @@ class FixedPointNumber:
             raise ValueError("scaling factor must be a positive integer.")
         self.internal = internal_value
         self.scale = scaling_factor
+
+    def _check(self, x):
+        if not (isinstance(x, FixedPointNumber)):
+            raise TypeError("value must be a FixedPointNumber.")
+        if not (x.scale == self.scale):
+            raise ValueError(
+                f"The scale factor is differ between {self.scale} and {x.scale}."
+            )
+
+    def add(self, x):
+        self._check(x)
+        self.internal += x.internal
+        return self
+
+    def sub(self, x):
+        self._check(x)
+        self.internal -= x.internal
+        return self
--- tests/test_fixed_point_number.py
+++ tests/test_fixed_point_number.py
@@ -16,5 +16,52 @@ def test_FixedPointNumber_constructor_000():
 def test_FixedPointNumber_constructor_100():
     with pytest.raises(Exception) as e:
         _ = FixedPointNumber(100, 0)
-
     assert str(e.value) == "scaling factor must be a positive integer."
+
+
+def test_FixedPointNumber_add_000():
+    x = FixedPointNumber(100, 100)
+    y = FixedPointNumber(100, 100)
+    z = x.add(y)
+    assert z.internal == 200
+    assert z.scale == 100
+
+
+def test_FixedPointNumber_add_100():
+    x = FixedPointNumber(100, 100)
+    y = 1
+    with pytest.raises(Exception) as e:
+        _ = x.add(y)
+    assert str(e.value) == "value must be a FixedPointNumber."
+
+
+def test_FixedPointNumber_add_101():
+    x = FixedPointNumber(100, 100)
+    y = FixedPointNumber(100, 10)
+    with pytest.raises(Exception) as e:
+        _ = x.add(y)
+    assert str(e.value) == "The scale factor is differ between 100 and 10."
+
+
+def test_FixedPointNumber_sub_000():
+    x = FixedPointNumber(100, 100)
+    y = FixedPointNumber(100, 100)
+    z = x.sub(y)
+    assert z.internal == 0
+    assert z.scale == 100
+
+
+def test_FixedPointNumber_sub_100():
+    x = FixedPointNumber(100, 100)
+    y = 1
+    with pytest.raises(Exception) as e:
+        _ = x.sub(y)
+    assert str(e.value) == "value must be a FixedPointNumber."
+
+
+def test_FixedPointNumber_sub_101():
+    x = FixedPointNumber(100, 100)
+    y = FixedPointNumber(100, 10)
+    with pytest.raises(Exception) as e:
+        _ = x.sub(y)
+    assert str(e.value) == "The scale factor is differ between 100 and 10."
