74bac3dba060c77f350c44ad35b35f1e2704a80a
kaznak <kaznak.at.work@gmail.com>
2022-03-19 21:15:27 +0900

    add

--- fixed_point_number/__init__.py
+++ fixed_point_number/__init__.py
@@ -1,1 +1,3 @@
+from .fixed_point_number import FixedPointNumber
+
 __version__ = "0.1.0"
--- tests/test_fixed_point_number.py
+++ tests/test_fixed_point_number.py
@@ -1,5 +1,9 @@
-from fixed_point_number import __version__
+from fixed_point_number import __version__, FixedPointNumber
 
 
 def test_version():
     assert __version__ == "0.1.0"
+
+
+def test_FixedPointNumber():
+    assert FixedPointNumber()
